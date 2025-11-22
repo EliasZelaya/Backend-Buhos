8a00664cef9f45bebf49954f3451ffaa6b87e9b8
EliasZelaya <00146322@uca.edu.sv>
2025-11-22 16:44:33 -0600

    add: controller to create subreview

---
+++ Backend-Buhos-Eat/src/main/java/com/example/eeza/backendbuhoseat/controllers/SubReviewController.java
@@ -0,0 +1,32 @@
+package com.example.eeza.backendbuhoseat.controllers;
+
+import com.example.eeza.backendbuhoseat.domain.dto.request.subreview.CreateSubReviewRequest;
+import com.example.eeza.backendbuhoseat.domain.dto.response.GeneralResponse;
+import com.example.eeza.backendbuhoseat.domain.entities.SubReview;
+import com.example.eeza.backendbuhoseat.services.SubReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+import static com.example.eeza.backendbuhoseat.utils.Constants.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(API+SUBREVIEW)
+public class SubReviewController {
+    private final SubReviewService subReviewService;
+
+    @PostMapping(CREATE+BY_ID_LOCAL)
+    public ResponseEntity<GeneralResponse> createSubReview(@Valid @RequestBody CreateSubReviewRequest createSubReviewRequest, @PathVariable UUID idLocal){
+        subReviewService.createSubReview(createSubReviewRequest, idLocal);
+        return GeneralResponse.builder()
+                .message(ENTITY_SUBREVIEW+CREATED)
+                .status(HttpStatus.CREATED)
+                .build()
+                .getResponse();
+    }
+}
