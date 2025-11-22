5a5e96f05b40ca407bc24ca213abbc30a70e2ded
EliasZelaya <00146322@uca.edu.sv>
2025-11-22 14:27:22 -0600

    add: subreview mapper create request to entity

--- Backend-Buhos-Eat/src/main/java/com/example/eeza/backendbuhoseat/utils/mappers/SubReviewMapper.java
+++ Backend-Buhos-Eat/src/main/java/com/example/eeza/backendbuhoseat/utils/mappers/SubReviewMapper.java
@@ -2,7 +2,9 @@ package com.example.eeza.backendbuhoseat.utils.mappers;
 
 import com.example.eeza.backendbuhoseat.domain.dto.request.subreview.CreateSubReviewRequest;
 import com.example.eeza.backendbuhoseat.domain.dto.response.subreview.SubReviewResponse;
+import com.example.eeza.backendbuhoseat.domain.entities.Review;
 import com.example.eeza.backendbuhoseat.domain.entities.SubReview;
+import com.example.eeza.backendbuhoseat.domain.entities.User;
 
 public class SubReviewMapper {
     public static SubReviewResponse toDtoResponse(SubReview subReview) {
@@ -11,6 +13,14 @@ public class SubReviewMapper {
                 .reviewId(subReview.getReview().getId())
                 .userId(subReview.getUser().getId())
                 .content(subReview.getContent())
+                .build();
+    }
+
+    public static SubReview toEntityCreate(CreateSubReviewRequest createSubReviewRequest, Review review, User user) {
+        return SubReview.builder()
+                .review(review)
+                .user(user)
+                .content(createSubReviewRequest.getContent())
                 .build();
     }
 }
